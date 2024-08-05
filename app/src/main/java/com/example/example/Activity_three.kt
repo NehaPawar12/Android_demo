20ae7bccf837a1d713d521508dd50f5ac2b58dd1
NehaPawar12 <nehapawar2320@gmail.com>
2024-08-05 14:04:13 +0530

    New 4th Activity button

--- app/src/main/java/com/example/example/Activity_three.kt
+++ app/src/main/java/com/example/example/Activity_three.kt
@@ -1,20 +1,31 @@
 package com.example.example
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Activity_three : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_three)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val web = findViewById<CardView>(R.id.cardweb)
+        val cam = findViewById<CardView>(R.id.cardcam)
+
+        web.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/in/neha-pawar-np1223/"))
+            startActivity(intent)
+        }
+
+        cam.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
         }
     }
 }
